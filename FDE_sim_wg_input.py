79f08e5440e8dffc88ba98361e32da39a82d4953
enricovallar <enrico.vallar@studenti.unipd.it>
2024-08-14 00:44:54 +0200

    Folder reorganization, H field added to extracted data

--- FDE_sim_wg_input.py
+++ FDE_sim_wg_input.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 
 
 
-PATH_MODELS = "models_wg_input"
+PATH_MODELS = "../models_wg_input"
 FILE_NAME   = "wg_width_"
 
 try:
@@ -62,8 +62,10 @@ for i,width in enumerate(width_array):
     env.groupscope("::model")
 
     sim = WaveguideModeFinder(env)
-    sim.add_simulation_region(width_simulation = 4*width, height_simulation = 4*(height_InP*height_SiN), number_of_trial_modes=20)
+    sim.add_simulation_region(width_simulation = 4* width, height_simulation=4*(height_InP+height_SiN), number_of_trial_modes=20)
     sim.add_mesh(width_mesh= 2*width, height_mesh=2*(height_InP+height_SiN), N=300)
+    
+
 
     print("Saving model before simulation...")
     env.save(f"{PATH_MODELS}/{FILE_NAME}{i}")
@@ -79,5 +81,60 @@ for i,width in enumerate(width_array):
 
     print("Saving model after simulation...")
     env.save(f"{FILE_NAME}{i}")
-    #env.close()
+    env.close()
+ # %%
+#___________________________________________________
+import pickle
+import os
+import importlib.util
+#The default paths for windows
+spec_win = importlib.util.spec_from_file_location('lumapi', 'C:\\Program Files\\Lumerical\\v242\\api\\python\\lumapi.py')
+#Functions that perform the actual loading
+lumapi = importlib.util.module_from_spec(spec_win) # 
+spec_win.loader.exec_module(lumapi)
+import numpy as np
+
+FOLDER_PATH = "../models_wg_input/"
+FILE_NAME = "wg_width_"
+SAVED_DATA_FOLDER = "../saved_data/"
+DATA_FILE_NAME = "wg_input_data.pickle"
+
+width_start = 250
+width_stop  = 1000
+width_step = 25
+width_array = np.arange(width_start, width_stop+width_step, width_step)*1e-9
+print(width_array)
+
+data_array = []
+
+env = lumapi.MODE()
+env.load(f"{FOLDER_PATH}{FILE_NAME}0")
+
+for i,width in enumerate(width_array):
+    data = []
+    for j in range(1, found_modes[i]+1):
+        
+        env.load(f"{FILE_NAME}{i}")
+        mode = "FDE::data::mode" + str(j)
+        try:       
+            extracted_data = (Analysis_wg.extract_data(env, mode))
+        except:
+            extracted_data = ({})
+        finally:
+            extracted_data["width"] = width
+            data.append(extracted_data)
+    data_array.append(data)
+    print(f"width {width} data collected")
+
+
+if os.path.exists(f"{SAVED_DATA_FOLDER}{DATA_FILE_NAME}"):
+    os.remove(f"{SAVED_DATA_FOLDER}{DATA_FILE_NAME}")
+with open(f"{SAVED_DATA_FOLDER}{DATA_FILE_NAME}", 'wb') as file:
+    pickle.dump({"width_array": width_array, 
+                 "found_modes": found_modes, 
+                 "data_array" : data_array},
+                 file)
+
+
+
 # %%
--- analysis_wg.py
+++ analysis_wg.py
@@ -29,6 +29,10 @@ class Analysis_wg:
         Ey = np.squeeze(env.getdata(mode, "Ey"))
         Ez = np.squeeze(env.getdata(mode, "Ez"))
 
+        Hx = np.squeeze(env.getdata(mode, "Hx"))
+        Hy = np.squeeze(env.getdata(mode, "Hy"))
+        Hz = np.squeeze(env.getdata(mode, "Hz"))
+
         data = { 
             "neff" : neff,
             "te_fraction" : te_fraction,
@@ -41,8 +45,12 @@ class Analysis_wg:
             "Ey": Ey,
             "Ez": Ez, 
 
+            "Hx": Hx, 
+            "Hy": Hy,
+            "Hz": Hz, 
+
             "E2": E2,
-            "H2": H2
+            "H2": H2, 
         }
 
         return data
